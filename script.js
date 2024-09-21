cea6b0eb02bb5bb29b080ae695fac64d129fc6c7
Muhammad361738 <ullahehsan016@gmail.com>
2024-09-21 13:43:55 +0500

    some changes

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const getData = async (myReqValue)=>{
      div.classList.add("detail");
      
     div.innerHTML = `
-             <h2>Word : <span>${jasonDat[0].word}</span></h2> 
-             <p> Meaning : <span> ${jasonDat[0].meanings[0].definitions[0].definition} 
-              <p> Example : <span> ${jasonDat[0].meanings[0].definitions[0].example} 
+             <h2>Word : <span>${jasonDat[0].word == undefined ? "Not Fond" : jasonDat[0].word }</span></h2> 
+             <p> Meaning : <span> ${jasonDat[0].meanings[0].definitions[0].definition == undefined ? "Not Fond" : jasonDat[0].meanings[0].definitions[0].definition } </span>
+              <p> Example : <span> ${jasonDat[0].meanings[0].definitions[0].example == undefined ? "Not Fond" : jasonDat[0].meanings[0].definitions[0].example  } </span> 
              <p>${jasonDat[0].meanings[0].partOfSpeech}</p>
-            <p>Synonyms :<span>${jasonDat[0].meanings[0].synonyms}</span></p>         
+            <p>Synonyms :<span>${jasonDat[0].meanings[0].synonyms  == undefined ? "Not Fond": jasonDat[0].meanings[0].synonyms }</span></p>         
             <audio controls>
-            <source src="${jasonDat[0].phonetics[0].audio}" type="audio/ogg">
+            <source src="${jasonDat[0].phonetics[0].audio == undefined ? "Not Fond":jasonDat[0].phonetics[0].audio }" type="audio/ogg">
               Your browser does not support the audio tag.
           </audio>
 
